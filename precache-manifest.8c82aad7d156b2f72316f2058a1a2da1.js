03b21be8f30600c65383e9c84a61120c5a490c08
Thien Nguyen <Thiennguyenwsb@gmail.com>
2019-03-25 16:42:25 -0700

    Updates

---
+++ precache-manifest.8c82aad7d156b2f72316f2058a1a2da1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b3b5b116e06bae3e6781e376159b385",
+    "url": "/quotes_machine/static/media/b-9.6b3b5b11.jpg"
+  },
+  {
+    "revision": "13275caa1354858281c1",
+    "url": "/quotes_machine/static/js/runtime~main.13275caa.js"
+  },
+  {
+    "revision": "0e7c051a04c71d617e5b",
+    "url": "/quotes_machine/static/js/main.0e7c051a.chunk.js"
+  },
+  {
+    "revision": "610d4d736b3be0ffa159",
+    "url": "/quotes_machine/static/js/2.610d4d73.chunk.js"
+  },
+  {
+    "revision": "0e7c051a04c71d617e5b",
+    "url": "/quotes_machine/static/css/main.db51aebb.chunk.css"
+  },
+  {
+    "revision": "0fb750d42211206e55383953f88a518c",
+    "url": "/quotes_machine/index.html"
+  }
+];
